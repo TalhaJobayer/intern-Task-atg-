f124beb78aeb6702af48972d95f777ff2c85456e
Talha Jobayer <talhajobayer@gmail.com>
2022-07-29 16:53:55 +0600

    responsive done

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import './Navbar.css'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
-const Navbar = () => {
+const Navbars = () => {
     return (
-        <nav class="navbar ">
-        <div class="container-fluid">
-          <a class="navbar-brand">ATGWORLD</a>
-          <form class="d-flex" role="search">
-            <input class="searchBar " type="search" placeholder=" Search for your favorite groups in ATG" aria-label="Search"/>
+       <Navbar bg="light" expand="lg" className='navbar'>
+      <Container fluid>
+        <Navbar.Brand href="#">ATGWORLD</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="d-flex justify-content-evenly">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+          </Nav>
+          <Form >
+            <Form.Control
+              type="search"
+              placeholder="Search for your favorite groups in ATG"
+              className="me-2 searchBar"
+              aria-label="Search"
+            />
            
-          </form>
-          <p>Create Account <span style={{color:"#2F6CE5"}}>Its Free</span></p>
-        </div>
-      </nav>
+          </Form>
+          <p className='createText'>Create Account <span style={{color:"#2F6CE5"}}>Its Free</span></p>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   
     );
 };
 
-export default Navbar;
+export default Navbars;
